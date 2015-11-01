69dd17c37b33b70e66f41dd380fb967338a5bb3e
Alex Pol <m-i-g-e-l@yandex.ru>
2015-11-01 23:46:20 +0300

    d

--- src/alex/pol/WebAppException.java
+++ src/alex/pol/WebAppException.java
@@ -8,7 +8,7 @@ import alex.pol.model.Resume;
 
 public class WebAppException extends RuntimeException {
     private Resume resume ;
-    private String uuid ;
+    private String uuid = null;
 
     public WebAppException(String message) {
         super(message);
